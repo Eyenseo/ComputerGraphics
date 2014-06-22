Eyenseo/ComputerGraphics

commit 4c8278f7fcb3504f0d7273e40f2b864c6bb4dd5e

Fix bad function name

src/button.cpp
@@ -189,8 +189,16 @@ void Button::on_press(unsigned int x, unsigned int y) {
   }
 }
 
-void Button::on_release(unsigned int x, unsigned int y) {
+void Button::on_click(unsigned int x, unsigned int y) {
   if(pressed_ && x > origin_[0] && y > origin_[1]
+     && x < origin_[0] + width_ * scale_[0]
+     && y < origin_[1] + height_ * scale_[1]) {
+    on_click_();
+  }
+}
+
+void Button::on_release(unsigned int x, unsigned int y) {
+  if(x > origin_[0] && y > origin_[1]
      && x < origin_[0] + width_ * scale_[0]
      && y < origin_[1] + height_ * scale_[1]) {
     on_release_();
@@ -199,6 +207,10 @@ void Button::on_release(unsigned int x, unsigned int y) {
 
 void Button::set_on_press(const std::function<void()>& f) {
   on_press_ = f;
+}
+
+void Button::set_on_click(const std::function<void()>& f) {
+  on_click_ = f;
 }
 
 void Button::set_on_release(const std::function<void()>& f) {

include/button.hpp
@@ -11,6 +11,7 @@ protected:
   unsigned int width_;
   unsigned int height_;
   std::function<void()> on_press_;
+  std::function<void()> on_click_;
   std::function<void()> on_release_;
   bool pressed_;
   GLVector<XYZW>& origin_ = Drawable::origin_;
@@ -27,10 +28,15 @@ public:
   virtual void draw();
 
   virtual void set_image(GLuint image);
+
   virtual void on_press(unsigned int x, unsigned int y);
   virtual void on_release(unsigned int x, unsigned int y);
+  virtual void on_click(unsigned int x, unsigned int y);
+
   virtual void set_on_press(const std::function<void()>& f);
   virtual void set_on_release(const std::function<void()>& f);
+  virtual void set_on_click(const std::function<void()>& f);
+
   virtual void set_pressed(bool pressed);
 
 };

src/main.cpp
@@ -586,7 +586,7 @@ void make_buttons(GLFWwindow*& window) {
   temp->set_color(0, 0, 1, 0);
   temp->set_color(1, 0, 0, 1);
   temp->set_scale(0.25);
-  temp->set_on_release([temp]() {
+  temp->set_on_click([temp]() {
     for(auto object : objects_) {
       delete object;
     }
@@ -600,7 +600,7 @@ void make_buttons(GLFWwindow*& window) {
       if(action == GLFW_PRESS) {
         temp->on_press(x, y);
       } else if(action == GLFW_RELEASE) {
-        temp->on_release(x, y);
+        temp->on_click(x, y);
       }
     }
   });
@@ -610,7 +610,7 @@ void make_buttons(GLFWwindow*& window) {
   temp->set_color(0, 0, 1, 0);
   temp->set_color(1, 0, 0, 1);
   temp->set_scale(0.25);
-  temp->set_on_release([temp]() {
+  temp->set_on_click([temp]() {
     // TODO use modelview to get a better position
     Sphere* s = new Sphere(0, 0, 7);
     objects_.push_front(s);
@@ -625,7 +625,7 @@ void make_buttons(GLFWwindow*& window) {
       if(action == GLFW_PRESS) {
         temp->on_press(x, y);
       } else if(action == GLFW_RELEASE) {
-        temp->on_release(x, y);
+        temp->on_click(x, y);
       }
     }
   });
@@ -635,7 +635,7 @@ void make_buttons(GLFWwindow*& window) {
   temp->set_color(0, 0, 1, 0);
   temp->set_color(1, 0, 0, 1);
   temp->set_scale(0.25);
-  temp->set_on_release([temp, window]() {
+  temp->set_on_click([temp, window]() {
     glfwSetWindowShouldClose(window, GL_TRUE);
   });
   mouse_callbacks_.push_front([temp](unsigned int button, unsigned int action,
@@ -644,7 +644,7 @@ void make_buttons(GLFWwindow*& window) {
       if(action == GLFW_PRESS || action == GLFW_REPEAT) {
         temp->on_press(x, y);
       } else if(action == GLFW_RELEASE) {
-        temp->on_release(x, y);
+        temp->on_click(x, y);
       }
     }
   });

commit 5c9373e38f17dbf3141d32936fbf74e5d52a0869

Fix bug introduced by 4c8278f7fcb3504f0d7273e40f2b864c6bb4dd5e

src/button.cpp
@@ -9,6 +9,7 @@ Button::Button()
     , width_(0)
     , height_(0)
     , on_press_([]() {})
+    , on_click_([]() {})
     , on_release_([]() {})
     , pressed_(false) {
 }
@@ -19,6 +20,7 @@ Button::Button(unsigned int origin_x, unsigned int origin_y,
     , width_(0)
     , height_(0)
     , on_press_([]() {})
+    , on_click_([]() {})
     , on_release_([]() {})
     , pressed_(false) {
   load_bmp(image_path);